7774e7b3578800b4a59036f8038b9ac379f757da
Simon Convent <simon@convnet.de>
2024-06-26 21:55:06 +0200

    Use makefile for test execution

--- examples/run.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-../build/tgc ./addition.toy > ./addition.ll
-#../build/tgc ./saxpy.toy > ./saxpy.ll
-
-/usr/lib/llvm-15/bin/llc ./addition.ll -o ./addition.ptx
-#/usr/lib/llvm-15/bin/llc ./saxpy.ll -o ./saxpy.ptx
